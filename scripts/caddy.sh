13366bd37360a6751f2a2a7c1f72b54c79e4f192
Michael Miranda <michaelmiranda161@gmail.com>
2023-05-02 04:44:09 -0400

    chore: replace blog subdomain reference

--- scripts/caddy.sh
+++ scripts/caddy.sh
@@ -2,11 +2,11 @@ cd ~/Developer/personal-website
 
 echo "Updating Caddy server with new config 🛠"
 echo "  Uploading Caddyfile to server..."
-scp Caddyfile root@blog.michaelmiranda.dev:/etc/caddy/michaelmiranda.dev
+scp Caddyfile root@michaelmiranda.dev:/etc/caddy/michaelmiranda.dev
 echo "  Caddyfile uploaded sucessfully 📝"
 
 echo "  Reloading Caddy server..."
-ssh root@blog.michaelmiranda.dev "systemctl reload caddy"
+ssh root@michaelmiranda.dev "systemctl reload caddy"
 echo "  Caddy reloaded successfully 🔁"
 echo "Caddy server updated 🎉"
 
--- scripts/release.sh
+++ scripts/release.sh
@@ -16,11 +16,11 @@ echo "  Minifying CSS..."
 echo " CSS Minified Successfully 🎨"
 
 echo "  Deleting existing code on server..."
-ssh root@blog.michaelmiranda.dev "rm -rf /home/mike/apps/personal-website"
+ssh root@michaelmiranda.dev "rm -rf /home/mike/apps/personal-website"
 echo "  Code deleted successfully ✅"
 
 echo "  Uploading code..."
-rsync -avr build/ root@blog.michaelmiranda.dev:/home/mike/apps/personal-website
+rsync -avr build/ root@michaelmiranda.dev:/home/mike/apps/personal-website
 echo "  Code uploaded successfully 📦"
 
 echo "  Cleaning up..."
@@ -29,14 +29,14 @@ rm -rf build
 echo "  Finished cleaning 🧹"
 
 echo "  Moving Caddyfile..."
-ssh root@blog.michaelmiranda.dev "cp /home/mike/apps/personal-website/Caddyfile /etc/caddy/michaelmiranda.dev"
+ssh root@michaelmiranda.dev "cp /home/mike/apps/personal-website/Caddyfile /etc/caddy/michaelmiranda.dev"
 echo "  Caddyfile moved successfully 🚚"
 
 echo "  Restarting Application server..."
-ssh root@blog.michaelmiranda.dev "service michaelmiranda.dev restart"
+ssh root@michaelmiranda.dev "service michaelmiranda.dev restart"
 echo "  Server restarted successfully 🔁"
 
 echo "  Reloading Caddy server..."
-ssh root@blog.michaelmiranda.dev "systemctl reload caddy"
+ssh root@michaelmiranda.dev "systemctl reload caddy"
 echo "  Caddy reloaded successfully 🔁"
 echo "Release finished 🚀"
