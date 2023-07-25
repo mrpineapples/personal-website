7bbed94f4ebe4b3c97bdc146fe303e07a55008b5
Michael Miranda <michaelmiranda161@gmail.com>
2023-07-25 00:41:58 -0400

    chore: move all controller logic to models

--- controllers/articles.go
+++ controllers/articles.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"errors"
 	"net/http"
 	"time"
 
@@ -10,6 +9,12 @@ import (
 	"github.com/mrpineapples/personal-website/models"
 	"github.com/mrpineapples/personal-website/utils"
 )
+
+var articleInput struct {
+	Title       string `form:"title"`
+	Description string `form:"description"`
+	Markdown    string `form:"markdown"`
+}
 
 func GetArticle(c *gin.Context) {
 	article, err := models.GetArticleBySlug(c.Param("slug"))
@@ -37,28 +42,7 @@ func GetArticle(c *gin.Context) {
 }
 
 func GetArticles(c *gin.Context) {
-	sqlStatement := `
-		SELECT id, title, description, markdown, slug, created_at, updated_at
-		FROM articles
-		ORDER BY created_at DESC;
-	`
-	rows, err := models.Pool.Query(models.DBContext, sqlStatement)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	var articles []models.Article
-	for rows.Next() {
-		var article models.Article
-		err = rows.Scan(&article.ID, &article.Title, &article.Description, &article.Markdown, &article.Slug, &article.CreatedAt, &article.UpdatedAt)
-		if err != nil {
-			panic(err)
-		}
-		articles = append(articles, article)
-	}
-
-	err = rows.Err()
+	articles, err := models.GetArticles()
 	if err != nil {
 		panic(err)
 	}
@@ -80,12 +64,6 @@ func NewArticle(c *gin.Context) {
 
 // CreateArticle creates a new article in our database
 func CreateArticle(c *gin.Context) {
-	var articleInput struct {
-		Title       string `form:"title"`
-		Description string `form:"description"`
-		Markdown    string `form:"markdown"`
-	}
-
 	if err := c.ShouldBind(&articleInput); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -97,66 +75,43 @@ func CreateArticle(c *gin.Context) {
 		Markdown:    articleInput.Markdown,
 		Slug:        slug.Make(articleInput.Title),
 	}
-
-	sqlStatement := `
-		INSERT INTO articles (title, description, markdown, slug)
-		VALUES ($1, $2, $3, $4)
-		RETURNING slug;
-	`
-	var slug string
-	err := models.Pool.QueryRow(models.DBContext, sqlStatement, article.Title, article.Description, article.Markdown, article.Slug).Scan(&slug)
+	err := models.CreateArticle(article)
 	if err != nil {
 		panic(err)
 	}
+
 	c.Redirect(http.StatusSeeOther, "/admin/blog")
 }
 
 func UpdateArticle(c *gin.Context) {
-	var articleInput struct {
-		Title       string `form:"title"`
-		Description string `form:"description"`
-		Markdown    string `form:"markdown"`
-	}
-
 	if err := c.ShouldBind(&articleInput); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	article := models.Article{
-		Title:       articleInput.Title,
-		Description: articleInput.Description,
-		Markdown:    articleInput.Markdown,
-		Slug:        slug.Make(articleInput.Title),
-		UpdatedAt:   time.Now(),
+	article, err := models.GetArticleByID(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
+	article.Title = articleInput.Title
+	article.Description = articleInput.Description
+	article.Markdown = articleInput.Markdown
+	article.Slug = slug.Make(articleInput.Title)
+	article.UpdatedAt = time.Now()
 
-	sqlStatement := `
-		UPDATE articles
-		SET slug = $2, title = $3, description = $4, markdown = $5, updated_at = $6
-		WHERE id = $1;
-	`
-	cmdTag, err := models.Pool.Exec(models.DBContext, sqlStatement, c.Param("id"), article.Slug, article.Title, article.Description, article.Markdown, article.UpdatedAt)
+	err = models.UpdateArticle(article)
 	if err != nil {
 		panic(err)
-	} else if cmdTag.RowsAffected() != 1 {
-		panic(errors.New("No row found to update"))
 	}
 
 	c.Redirect(http.StatusSeeOther, "/blog/"+article.Slug)
 }
 
 func DeleteArticle(c *gin.Context) {
-	sqlStatement := `
-		DELETE FROM articles
-		WHERE id = $1;
-	`
-
-	cmdTag, err := models.Pool.Exec(models.DBContext, sqlStatement, c.Param("id"))
+	err := models.DeleteArticle(c.Param("id"))
 	if err != nil {
 		panic(err)
-	} else if cmdTag.RowsAffected() != 1 {
-		panic(errors.New("No row found to delete"))
 	}
+
 	c.Redirect(http.StatusSeeOther, "/admin/blog")
 }
--- models/article.go
+++ models/article.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
 	"time"
@@ -29,16 +30,37 @@ func (a *Article) ToHTML() (template.HTML, error) {
 	return template.HTML(articleBuf.String()), nil
 }
 
+func GetArticleByID(ID string) (Article, error) {
+	sqlStatement := `
+		SELECT id, title, description, markdown, slug, created_at, updated_at
+		FROM articles
+		WHERE id = $1;
+	`
+
+	article := Article{}
+	row := Pool.QueryRow(DBContext, sqlStatement, ID)
+	err := row.Scan(&article.ID, &article.Title, &article.Description, &article.Markdown, &article.Slug, &article.CreatedAt, &article.UpdatedAt)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			fmt.Println(`No rows were returned for the following ID:`, ID)
+		}
+		return Article{}, err
+	}
+
+	return article, nil
+}
+
 func GetArticleBySlug(slug string) (Article, error) {
 	sqlStatement := `
-		SELECT title, description, markdown, created_at, updated_at
+		SELECT id, title, description, markdown, slug, created_at, updated_at
 		FROM articles
 		WHERE slug = $1;
 	`
 
 	article := Article{}
 	row := Pool.QueryRow(DBContext, sqlStatement, slug)
-	err := row.Scan(&article.Title, &article.Description, &article.Markdown, &article.CreatedAt, &article.UpdatedAt)
+	err := row.Scan(&article.ID, &article.Title, &article.Description, &article.Markdown, &article.Slug, &article.CreatedAt, &article.UpdatedAt)
 
 	if err != nil {
 		if err == pgx.ErrNoRows {
@@ -48,4 +70,78 @@ func GetArticleBySlug(slug string) (Article, error) {
 	}
 
 	return article, nil
+}
+
+func GetArticles() ([]Article, error) {
+	sqlStatement := `
+		SELECT id, title, description, markdown, slug, created_at, updated_at
+		FROM articles
+		ORDER BY created_at DESC;
+	`
+	rows, err := Pool.Query(DBContext, sqlStatement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	articles := make([]Article, 0)
+	for rows.Next() {
+		article := Article{}
+		err = rows.Scan(&article.ID, &article.Title, &article.Description, &article.Markdown, &article.Slug, &article.CreatedAt, &article.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, article)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
+
+func CreateArticle(article Article) error {
+	sqlStatement := `
+		INSERT INTO articles (title, description, markdown, slug)
+		VALUES ($1, $2, $3, $4);
+	`
+	_, err := Pool.Exec(DBContext, sqlStatement, article.Title, article.Description, article.Markdown, article.Slug)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateArticle(article Article) error {
+	sqlStatement := `
+		UPDATE articles
+		SET slug = $2, title = $3, description = $4, markdown = $5, updated_at = $6
+		WHERE id = $1;
+	`
+	cmdTag, err := Pool.Exec(DBContext, sqlStatement, article.ID, article.Slug, article.Title, article.Description, article.Markdown, article.UpdatedAt)
+	if err != nil {
+		return err
+	} else if cmdTag.RowsAffected() != 1 {
+		return errors.New("No row found to update")
+	}
+
+	return nil
+}
+
+func DeleteArticle(ID string) error {
+	sqlStatement := `
+		DELETE FROM articles
+		WHERE id = $1;
+	`
+	cmdTag, err := Pool.Exec(DBContext, sqlStatement, ID)
+	if err != nil {
+		return err
+	} else if cmdTag.RowsAffected() != 1 {
+		return errors.New("No row found to delete")
+	}
+
+	return nil
 }
